b2710ab927c2c58943d1041c23a2d4bf8452be65
LiuEnTzu <tainn_50554@yahoo.com.tw>
2020-04-13 23:13:13 +0800

    add password_confirmation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,19 +4,15 @@ class UsersController < ApplicationController
   end
 
   def sign_up
+    @user = User.new
   end
   
   def registration
-    if user_params[:password] == user_params[:password_confirm]
-      u = User.new(user_params)
-      
-      if u.save
-        redirect_to "/"
-      else
-        redirect_to "/sign_up"
-      end
-    else 
-      redirect_to "/sign_up"
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to "/"
+    else
+      render :sign_up 
     end
   end
 
@@ -24,7 +20,7 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:email, 
                                  :password, 
-                                 :password_confirm)
+                                 :password_confirmation)
   end
 
 end
