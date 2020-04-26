17c03cc0f11badd1e1ceed26d566e758fdec75de
LiuEnTzu <tainn_50554@yahoo.com.tw>
2020-04-26 18:32:05 +0800

    add column

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,8 @@ class Item < ApplicationRecord
   validates :name, presence: true
   validates :price, presence: true, 
                     numericality: { greater_than: 0 }
+
+  belongs_to :category 
+
+
 end
