f6580931c116712c3e55b0d2c2b488ef6843c4ef
LiuEnTzu <tainn_50554@yahoo.com.tw>
2020-05-02 13:07:39 +0800

    add test1-1

---
+++ app/models/cart.rb
@@ -0,0 +1,8 @@
+class Cart
+  def add_item(item_id)
+  end
+
+  def emtpy?
+    false
+  end
+end
---
+++ spec/models/cart_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Cart, type: :model do
+  describe "基本功能" do
+    it "可以把商品丟進購物車裡，然後購物車裡就有東西了" do
+      cart = Cart.new
+      cart.add_item(1)
+      expect(cart.emtpy?).to be false
+    end
+
+  end
+
+  describe "進階功能" do
+  end
+end
