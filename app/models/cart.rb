7d71dbe0664ecdbe6df6feaf854ad8cb960ad770
LiuEnTzu <tainn_50554@yahoo.com.tw>
2020-05-02 13:16:34 +0800

    test1-2

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,8 +1,17 @@
 class Cart
+  def initialize
+    @items = []
+  end
+
   def add_item(item_id)
+    @items << item_id
   end
 
   def emtpy?
-    false
+    @items.emtpy?
+  end
+
+  def items
+    @items
   end
 end
